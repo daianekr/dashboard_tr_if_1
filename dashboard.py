93c533ec33b5ed9c3e4cf66e5d2a081bd0016161
daianekr <daianekreitlow@gmail.com>
2024-02-28 21:41:51 -0300

    add filter initial_sem

--- dashboard.py
+++ dashboard.py
@@ -6,4 +6,11 @@ import plotly.express as px
 st.set_page_config(layout="wide")
 
 # Carregar os dados do arquivo "vendas.csv" (certifique-se de que o arquivo está no mesmo diretório)
-df = pd.read_csv("matricula_teste.csv", sep=";")
+df = pd.read_csv("Dados_2012_1_2023_1_DC.csv", sep=",")
+
+initial_sem = st.sidebar.selectbox("Selecione o Peródo de Início", df["Semestre_Ini"].unique())
+
+df_filtered = df[df["Semestre_Ini"] == initial_sem]
+
+# Exibir o DataFrame filtrado
+st.write(df_filtered)
