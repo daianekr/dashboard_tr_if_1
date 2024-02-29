7cae2429e16679298a3002dbf855c9ea5876002f
daianekr <daianekreitlow@gmail.com>
2024-02-28 21:48:16 -0300

    add dynamic filters

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from streamlit_dynamic_filters import DynamicFilters
 
 # Configurar o layout da página
 st.set_page_config(layout="wide")
@@ -8,9 +9,14 @@ st.set_page_config(layout="wide")
 # Carregar os dados do arquivo "vendas.csv" (certifique-se de que o arquivo está no mesmo diretório)
 df = pd.read_csv("Dados_2012_1_2023_1_DC.csv", sep=",")
 
-initial_sem = st.sidebar.selectbox("Selecione o Peródo de Início", df["Semestre_Ini"].unique())
+dynamic_filters = DynamicFilters(df=df, filters=['Instituicao', 'Descricao_Curso','Semestre_Ini','Semestre_fim'])
+                                 
+dynamic_filters.display_filters(location='sidebar')
+dynamic_filters.display_df()                            
 
-df_filtered = df[df["Semestre_Ini"] == initial_sem]
+# initial_sem = st.sidebar.selectbox("Selecione o Peródo de Início", df["Semestre_Ini"].unique())
 
-# Exibir o DataFrame filtrado
-st.write(df_filtered)
+# df_filtered = df[df["Semestre_Ini"] == initial_sem]
+
+# # Exibir o DataFrame filtrado
+# st.write(df_filtered)
